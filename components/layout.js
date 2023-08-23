23e80198f521c021e2f41b3d83e620c96c5e0023
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-08-23 22:05:53 +0900

    hotfix

--- components/layout.js
+++ components/layout.js
@@ -35,7 +35,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile2.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -53,7 +53,7 @@ export default function Layout({ children, home }) {
               {/* ただし、Next.js外のページに遷移するにはaタグを使わないといけない */}
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/profile2.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
